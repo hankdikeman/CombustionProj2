1e0c326d924bc0aa26b50127819e2eb230adf676
hankdikeman <dikem003@umn.edu>
2021-02-26 23:34:51 -0600

    fixed CSTR code bug

--- CSTR_sim1.py
+++ CSTR_sim1.py
@@ -8,7 +8,8 @@ import matplotlib.pyplot as plt
 
 # create the gas mixture and composition arrays
 gas = ct.Solution('gri30.xml')
-residence_times = [np.power((x + 1) / 20, 2) for x in range(20)]
+gas2 = ct.Solution('gri30.xml')
+residence_times = [(x + 0.01) / 50 for x in range(500)]
 print("res times:", residence_times)
 NO_conc = np.zeros_like(residence_times)
 CO_conc = np.zeros_like(residence_times)
@@ -18,10 +19,11 @@ for n, t in enumerate(residence_times):
     print('res_time:', t)
     # pressure = 60 Torr, T = 750 K
     p = ct.one_atm
-    T = 750
+    T = 650
     phi = 0.85
 
     gas.TPX = T, p, {'CH4': phi, 'O2': 2 / phi, 'N2': 2 * 3.76 / phi}
+    gas2.TPX = 2000, p, {'CH4': phi, 'O2': 2 / phi, 'N2': 2 * 3.76 / phi}
 
     # create an upstream reservoir that will supply the reactor. The temperature,
     # pressure, and composition of the upstream reservoir are set to those of the
@@ -29,7 +31,7 @@ for n, t in enumerate(residence_times):
     upstream = ct.Reservoir(gas)
 
     # Now create the reactor object with the same initial state
-    cstr = ct.IdealGasReactor(gas)
+    cstr = ct.IdealGasReactor(gas2)
 
     # Set its volume to 1000 cm^3. In this problem, the reactor volume is fixed, so
     # the initial volume is the volume at all later times.
@@ -38,7 +40,7 @@ for n, t in enumerate(residence_times):
     # Connect the upstream reservoir to the reactor with a mass flow controller
     # (constant mdot). Set the mass flow rate to 1.25 sccm.
     res_time = t
-    mdot = res_time * cstr.volume * gas.density
+    mdot = cstr.volume * gas.density / res_time
     print("mdot:", mdot, '\n')
     sccm = 1.25
     vdot = sccm * 1.0e-6 / 60.0 * \
@@ -60,13 +62,14 @@ for n, t in enumerate(residence_times):
 
     # now integrate in time
     time = 0.0
-    dt = 0.1
+    dt = 1
 
     states = ct.SolutionArray(gas, extra=['t'])
-    while time < 400.0:
+    while time < 2500.0:
         time += dt
         network.advance(time)
-        states.append(cstr.thermo.state, t=time)
+
+    states.append(cstr.thermo.state, t=time)
 
     NO_conc[n] = states('NO').X[-1]
     CO_conc[n] = states('CO').X[-1]
@@ -74,23 +77,32 @@ for n, t in enumerate(residence_times):
 species = ['CO', 'NO']
 
 if __name__ == '__main__':
+    gas.TPX = 650, ct.one_atm, {'CH4': phi,
+                                'O2': 2 / phi, 'N2': 2 * 3.76 / phi}
+    gas.equilibrate('HP')
+    print('NO 10s:', NO_conc[-1])
+    print('CO 10s:', CO_conc[-1])
+    print('Eq NO:', gas['NO'].X)
+    print('Eq CO:', gas['CO'].X)
+
     plt.figure(1)
-    plt.plot(residence_times, NO_conc, 'ro-', label='[NO]$_{SS}$')
+    plt.plot(residence_times, NO_conc / gas['NO'].X, 'r-', label='[NO]$_{SS}$')
     plt.title('NO Production as a Function of Residence Time')
     plt.legend(loc='upper right')
     plt.xlabel('time [s]')
-    plt.yscale('log')
-    plt.ylabel('mol fraction')
+    # plt.yscale('log')
+    plt.ylabel('Normalized NO Concentration ([NO]/[NO]${eq}$)')
+    plt.ylim(0, 1)
     figname = 'Figures/Sim3/NOProd.png'
     plt.savefig(figname)
     plt.show()
     plt.figure(2)
-    plt.plot(residence_times, CO_conc, 'bo-', label='[CO]$_{SS}$')
+    plt.plot(residence_times, CO_conc / gas['CO'].X, 'b-', label='[CO]$_{SS}$')
     plt.title('CO Production as a Function or Residence Time')
     plt.legend(loc='upper right')
     plt.xlabel('time [s]')
-    plt.yscale('log')
+    # plt.yscale('log')
     figname = 'Figures/Sim3/COProd.png'
     plt.savefig(figname)
-    plt.ylabel('mol fraction')
+    plt.ylabel('Normalized CO Concentration ([CO]/[CO]${eq}$)')
     plt.show()
